df7acfb153be5414111524a7f078afd05fc918f3
augustorochasandrini <augustosandrini2@gmail.com>
2022-09-14 18:47:22 -0300

    commit inicial

---
+++ src/test/java/br/com/programacaoempar/springbootv1/SpringBootV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.programacaoempar.springbootv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
