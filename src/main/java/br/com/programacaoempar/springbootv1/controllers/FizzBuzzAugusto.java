61b343bc4f554425f4a962f1f12a7f17e6b3d891
augustorochasandrini <augustosandrini2@gmail.com>
2022-09-14 19:01:55 -0300

    regras gerais

--- src/main/java/br/com/programacaoempar/springbootv1/controllers/FizzBuzzAugusto.java
+++ src/main/java/br/com/programacaoempar/springbootv1/controllers/FizzBuzzAugusto.java
@@ -2,17 +2,22 @@ package br.com.programacaoempar.springbootv1.controllers;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/augusto/fizzbuzz")
+@RequestMapping("/augusto/fizzbuzz/{numero}")
 public class FizzBuzzAugusto {
 
 
+    //primeira regra: se o numero devisivel por 3 retorna fizz
+    //segunda regra: se o numero divisivel por 5 retorna buzz
+    //terceira regra: se o numero divisivel por 3 e por 5 retorna fizzbuzz
+    //quarta regra: se o numero nao é divisivel por nenhum deles retorna o numero recebido
     @GetMapping
-    public String executa(){
-        return "OK";
+    public String executa(@PathVariable("numero") Integer numero){
+        return String.valueOf(numero);
     }
 
 }
